25bee322477d2fcea5b18316b17591e1b45ac8ee
Rogers, Leah <lrogers@visa.com>
2017-08-06 11:00:16 -0600

    added a new simple project

---
+++ SimpleDemoProject/SimpleDemoProject.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleDemoProject;
+
+namespace SimpleDemoProject.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Program program = new Program();
+        [TestMethod]
+        public void additionTest()
+        {
+
+            Assert.AreEqual(program.addition(1, 3), 4);
+        }
+
+        [TestMethod]
+        public void additionTest2()
+        {
+            Assert.AreNotEqual(program.addition(1, 3), 5);
+        }
+    }
+}
---
+++ SimpleDemoProject/SimpleDemoProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDemoProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine(program.addition(1, 2));
+            Console.ReadLine();
+        }
+
+        public  int addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public  int subtraction(int num1, int num2)
+        {
+            if(num1 >= num2)
+            {
+                return num1 - num2;
+            }
+            else
+            {
+                return num2 - num1;
+            }
+        }
+
+        public int multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
